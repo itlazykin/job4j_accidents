2fd7669c3fa1a0bcc95509bcca2de2ad7ef496b7
itlazykin <dzaka89@gmail.com>
2025-01-27 11:49:43 +0300

    0. Spring Security [#6879]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,15 +1,13 @@
 package ru.job4j.accidents.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping({"/", "/index"})
-    public String getIndex(Model model) {
-        model.addAttribute("user", "Denis Lazykin");
+    public String getIndex() {
         return "index";
     }
 }
